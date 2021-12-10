1507907f55e5f79ad65d579352c30827a15166da
masterStormtrooper <yignchengluo@gmail.com>
2021-12-10 10:33:51 -0600

    added scripts

---
+++ scirpts/dentate_withdays.py
@@ -0,0 +1,70 @@
+from numpy.matrixlib.defmatrix import matrix
+import pandas as pd
+import scanpy as sc
+import numpy as np
+from VITAE import VITAE
+from VITAE.utils import load_data
+import tensorflow as tf
+import random
+import os
+
+
+def create_heatmap_matrix(pi):
+    """Create heatmap matrix
+    @pi: numpy array contains pi weights"""
+    n = pi.shape[0]
+    matrix = np.zeros_like((n, n))
+    matrix[np.triu_indices(n)] = pi
+    mask = np.tril(np.ones_like(matrix), k=-1)
+    return matrix, mask
+
+
+def reset_random_seeds(seed):
+   os.environ['PYTHONHASHSEED']=str(seed)
+   tf.random.set_seed(seed)
+   np.random.seed(seed)
+   random.seed(seed)
+
+
+def run(seed, data, name, fp):
+    """
+    @seed: int seed number
+    @data: scanpy object"""
+    reset_random_seeds(seed)
+    tf.keras.backend.clear_session() 
+    model = VITAE()
+    model.initialize(adata = data, covariates='days', model_type = 'Gaussian')
+    model.pre_train() 
+    model.init_latent_space(cluster_label= 'leiden', res = 0.4, pilayer=True) 
+    
+    model.train()
+    p = model.vae.pilayer
+    for x in data.obs['days'].unique():
+        tmp = tf.expand_dims(tf.constant([x], dtype=tf.float32), 0)
+        tf.nn.softmax(p(tmp)).numpy()[0].save(fp + f'day:{str(x)}-run{name}')
+    sc.tl.umap(model._adata_z)
+    model._adata.obsp = model._adata_z.obsp
+    model._adata.obsm = model._adata_z.obsm
+    model._adata.write_h5ad(fp + f'modeladata-run{name}')
+    
+if __name__ == "__main__":
+    # load data
+    fp = ''
+    data = load_data("data/", file_name="dentate_withdays")
+    labels = pd.DataFrame({'Grouping': data['grouping']}, index = data['cell_ids'])
+    labels['Grouping'] = labels['Grouping'].astype("category")
+    genes = pd.DataFrame({'gene_names': data['gene_names']}, index = data['gene_names'])
+    dd = sc.AnnData(X = data['count'].copy(), obs = labels, var = genes)
+    dd.layers['counts'] = dd.X.copy()
+    dd.obs['days'] = data['covariates']
+    sc.pp.normalize_total(dd, target_sum=1e4)
+    sc.pp.log1p(dd)
+    sc.pp.highly_variable_genes(dd, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    sc.pp.scale(dd, max_value=10)
+    sc.tl.pca(dd, svd_solver='arpack')
+    sc.pp.neighbors(dd, n_neighbors=10, n_pcs=40)
+    sc.tl.leiden(dd, resolution = 0.4)
+    sc.tl.umap(dd)
+    for seed in range(20):
+        run(seed, data, seed, fp)
+
---
+++ scirpts/mousebrain.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import scanpy as sc
+import numpy as np
+import os as os
+import sys
+sys.path.append('/home/alanluo/vitae_git/VITAE')
+import VITAE
+from VITAE.utils import load_data
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+data = load_data(path = "/project/jingshuw/trajectory_analysis/data", file_name = "mouse_brain_merged")
+labels = pd.DataFrame({'Grouping': data['grouping']}, index = data['cell_ids'])
+labels['Grouping'] = labels['Grouping'].astype("category")
+genes = pd.DataFrame({'gene_names': data['gene_names']}, index = data['gene_names'])
+dd = sc.AnnData(X = data['count'], obs = labels, var = genes)
+#dd.layers['counts'] = dd.X.copy()
+dd.obs['Source'] = data['covariates'][:, 2]
+dd.obs['S_Score'] = data['covariates'][:, 0]
+dd.obs['G2M_Score'] = data['covariates'][:, 1]
+dd.obs['Day'] = np.array([item[0] for item in np.char.split(data['cell_ids'], sep ='_')])
+sc.pp.normalize_total(dd, target_sum=1e4)
+sc.pp.log1p(dd)
+sc.pp.highly_variable_genes(dd, min_mean=0.0125, max_mean=3, min_disp=0.5)
+sc.pp.scale(dd, max_value=10)
+sc.tl.pca(dd, svd_solver='arpack')
+sc.pp.neighbors(dd, n_neighbors=10, n_pcs=40)
+sc.tl.leiden(dd, resolution = 0.4)
+### Scanpy visualization ###
+sc.tl.umap(dd)
+sc.pl.umap(dd, color=['Grouping', 'Source', 'Day', 'S_Score', 'G2M_Score'], legend_loc='on data', legend_fontsize=5)
+
+
+model = VITAE.VITAE()
+
+model.initialize(adata = dd,
+                 npc = 64, model_type = 'Gaussian',
+                 hidden_layers = [32], latent_space_dim = 16,
+                 covariates = ['Source', 'S_Score', 'G2M_Score'])
+                 
+
+
+model.pre_train(early_stopping_tolerance = 0.01, early_stopping_relative = True)
+model.visualize_latent(color=['Grouping', 'Source', 'Day', 'S_Score', 'G2M_Score'], method = "UMAP")
+
+model.init_latent_space(res = 0.6, ratio_prune = 0.5)
+model.visualize_latent(color=['Grouping', 'Source', 'Day', 'S_Score', 'G2M_Score', 'vitae_init_clustering', 'Eomes'], method = "UMAP")
+
+model.train( early_stopping_tolerance = 0.01, early_stopping_relative = True)
+model.init_inference(batch_size=32, L=10)
+model.visualize_latent(color=['Grouping', 'Source', 'Day', 'S_Score', 'G2M_Score', 'vitae_init_clustering', 'vitae_new_clustering', 'Eomes'], method = "UMAP")
+
+G = model.comp_inference_score(method='modified_map',  # 'mean', 'modified_mean', 'map', and 'modified_map'
+                               no_loop=True,            # if no_loop=True, then find the maximum spanning tree
+                               cutoff = 0.1)
+G.edges(data = True)
+
+import matplotlib.pyplot as plt
+plt.show()
+
+model.infer_trajectory(init_node=0,  # initial node for computing pseudotime.
+                       cutoff=0.1                 # (Optional) cutoff score for edges (the default is 0.01).
+                       )  
+model.visualize_latent(color = ['vitae_new_clustering', 'Grouping', 'leiden','pseudotime', 
+                                'projection_uncertainty'], method = "UMAP")
+                                
+model.visualize_latent(color = ['vitae_new_clustering', 'clusters', 'pseudotime',
+                                'projection_uncertainty'], method = "UMAP")
